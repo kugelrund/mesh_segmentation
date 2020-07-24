Fix normalization of kmeans input
Not the eigenvectors are supposed to be normalized, but the observables
for kmeans. So if the eigenvectors are in the columns of V, then the
rows of V have to be normalized.

src/mesh_segmentation/segmentation.py
@@ -175,8 +175,8 @@ def segment_mesh(mesh, k, coefficients, action, ev_method):
         _, V = scipy.linalg.eigh(L, eigvals = (L.shape[0] - k, L.shape[0] - 1))
     else:
         _, V = scipy.sparse.linalg.eigsh(L, k)
-    # normalize each column to unit length
-    V = V / [numpy.linalg.norm(column) for column in V.transpose()]
+    # normalize each row to unit length
+    V /= numpy.linalg.norm(V, axis=1)[:,None]
 
     print("mesh_segmentation: Preparing kmeans...")
     # compute association matrix